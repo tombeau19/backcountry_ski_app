31b7b0f8bf19a3e4ed8489ad0b2cfa28e3bdf2e4
tombeau19 <thomas.beauregard@gmail.com>
2017-09-30 10:23:28 -0400

    styled landing page

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,10 @@
 var express = require('express');
 var router = express.Router();
 
+/*$(document).ready(() => {
+  $('.parallax').parallax();
+});*/
+
 /* GET home page. */
 router.get('/', (req, res) => {
   res.render('index', { title: 'Backcountry Buddy' });
